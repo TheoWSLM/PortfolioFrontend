5d70026c942d682373b2715ded0f343d35014a9f
TheoWSLM <theoweber1@gmail.com>
2024-02-02 01:56:30 -0300

    Feat: Mudando fonte da aplicação

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import 'typeface-poppins'
 
 const app = createApp(App)
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ export default {
     './src/**/*.{js,jsx,ts,tsx,vue}',
     "./node_modules/flowbite/**/*.js"
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
