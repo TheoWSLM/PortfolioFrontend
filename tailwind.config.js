e4e2217f3647bd6afce931955355f604ad5e2f12
TheoWSLM <theoweber1@gmail.com>
2024-02-01 23:51:04 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+    "./node_modules/flowbite/**/*.js"
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
