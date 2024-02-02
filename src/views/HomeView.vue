6da2408e3c58cf3d2aa4807a4b9129305e55ee22
TheoWSLM <theoweber1@gmail.com>
2024-02-02 17:38:39 -0300

    feat: Criação e novos componentes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,14 @@
 <script setup>
+import AboutSection from '@/components/AboutSection.vue';
 import Banner from '@/components/Banner.vue';
+import ExperienceSection from '@/components/ExperienceSection.vue';
+import TecnologySection from'@/components/TecnologySection.vue';
 
 </script>
 
 <template>
 <Banner></Banner>
+<TecnologySection></TecnologySection>
+<ExperienceSection></ExperienceSection>
+<AboutSection></AboutSection>
 </template>
