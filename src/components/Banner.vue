dd7ae1a97da9640da0192ebebaa287917f73e3af
TheoWSLM <theoweber1@gmail.com>
2024-02-02 23:39:42 -0300

    refactor: alterações de seções e definição de cor principal indigo

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,20 +1,24 @@
 <template>
 <main class="relative ">
   <div class=" inset-0">
-    <div class="absolute inset-0 bg-gradient-to-b from-blue-200 to-purple-300"></div>
+    <div class="absolute inset-0 bg-gradient-to-b from-white to-indigo-200"></div>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="absolute bottom-0 left-0 w-full">
       <path fill="#ffffff" fill-opacity="1" d="M0,224L48,229.3C96,235,192,245,288,218.7C384,192,480,128,576,122.7C672,117,768,171,864,208C960,245,1056,267,1152,240C1248,213,1344,139,1392,101.3L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
     </svg>
   </div>
   <div class="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 relative">
     <div class="flex flex-col mt-6 justify-between lg:flex-row">
-      <div class="mb-12 lg:max-w-lg lg:pr-5 lg:mb-0">
-        <div class="max-w-xl mb-6 lg:w-8/12">
+      <div class="mb-12 lg:mb-0">
+        <div class="max-w-xl mb-6 ">
           <h2 class="mt-36 text-7xl font-bold lg:text-8xl lg:tracking-tight">
-            Theo Weber
+            Theo 
+            <br>Weber S.</br>
           </h2>                          
           <p class="mt-4 pl-4 border-l-2 border-gray-500 text-sm text-gray-500 md:text-base">
-        Aqui conto a história de como transformo código em inovação, onde cada projeto é um passo em direção a um futuro mais humano e mais tecnológico. Vamos juntos!
+        Transformo código em inovação. Cada projeto 
+        <br>é um passo em direção a um futuro mais  </br>
+        <br> humano, mais tecnológico.</br> 
+        <br>Vamos juntos!</br>
           </p>            
         </div>      
       </div>
@@ -26,3 +30,4 @@
   </div>
 </main>
 </template>
+<!-- emerald200 ou indico 200 -->
